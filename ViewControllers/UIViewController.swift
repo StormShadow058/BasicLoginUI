d1c87252dcf8956dfdd421964bf60e5a70cfb0b3
Storm Shadow <83181647+StormShadow058@users.noreply.github.com>
2022-01-21 02:12:08 +0530

    update on login background

--- ViewControllers/UIViewController.swift
+++ ViewControllers/UIViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    var player:AVPlayer?
+    
+    var playerLayer:AVPlayerLayer?
     
     @IBOutlet weak var signupButton: UIButton!
     
@@ -23,10 +27,39 @@ class ViewController: UIViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        setUpVid()
+    }
+    
     func setUpElements() {
         Utilities.styleFilledButton(signupButton)
         Utilities.styleHollowButton(loginButton)
     }
 
-
+    func setUpVid() {
+        
+        let bPath = Bundle.main.path(forResource: "loginbg", ofType: "mp4")
+        
+        guard bPath != nil else {
+            return
+        }
+        
+        let url = URL(fileURLWithPath: bPath!)
+        
+        
+        let item = AVPlayerItem(url: url)
+        
+        player = AVPlayer(playerItem: item)
+        
+        playerLayer = AVPlayerLayer(player: player!)
+        
+        playerLayer?.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        
+        view.layer.insertSublayer(playerLayer!, at: 0)
+        
+        player?.playImmediately(atRate: 1)
+        
+    }
+    
+    
 }
