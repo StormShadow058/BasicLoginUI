ba0d64f69fdcc764b462aafabf9f73eef01b6b1f
Storm Shadow <83181647+StormShadow058@users.noreply.github.com>
2022-01-28 17:54:09 +0530

    update

--- Helpers/Utilities.swift
+++ Helpers/Utilities.swift
@@ -39,10 +39,9 @@ class Utilities {
     static func styleHollowButton(_ button:UIButton) {
         
         // Hollow rounded corner style
-        button.layer.borderWidth = 2
-        button.layer.borderColor = UIColor.black.cgColor
+        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
         button.layer.cornerRadius = 25.0
-        button.tintColor = UIColor.black
+        button.tintColor = UIColor.white
     }
     
     static func isPasswordValid(_ password : String) -> Bool {
